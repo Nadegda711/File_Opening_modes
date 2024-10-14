174fe85e24e99b6094890b72535b98d9340c95ee
Nadezda711 <semiliya711@mail.ru>
2024-10-14 05:06:33 +0500

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -9,9 +9,11 @@ class Product:
 
 class Shop:
     __file_name = 'products.txt'
+    file = open(__file_name, 'w')
+
 
     def get_products(self):
-        file = open(self.__file_name, 'r')
+        file = open(self.__file_name)
         prod_str = file.read()
         file.close()
         return prod_str
@@ -24,20 +26,19 @@ class Shop:
                 file.write(f'{product}\n')
                 file.close()
             else:
-                print(f'Продукт {product.name} уже есть в магазине')
-                break
+                print(f'Продукт {product} уже есть в магазине')
 
-
+    file.close()
 
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
-#
+
 print(p2) # __str__
-#
+
 s1.add(p1, p2, p3)
-#
+
 print(s1.get_products())
-#
-#
+
+
